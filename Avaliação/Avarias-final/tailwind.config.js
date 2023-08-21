a3370e2ce2ccd3568f05851dd9bdbe57d1bc353a
Riany Mello <mello.riany@gmail.com>
2023-08-21 02:17:28 +0100

    commit

---
+++ Avaliação/Avarias-final/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        bgBlue: '#030B39',
+        bgPurple:  'rgba(85, 36, 130, 0.5)',
+      },
+    },
+  },
+  plugins: [],
+}
